a3e12c32cc3a616f913185ec08d8866159dcf337
Adam <schrock-adam@hotmail.com>
2023-03-22 13:58:17 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {/* Header */}
+      {/* Categories section */}
+      {/* Featured Products section */}
+      {/* Shop now card (Photo and description) */}
+      {/* trending items */}
+      {/* footer */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <p>Categories</p>
+    </div>
+  );
+}
+
+export default Header;
