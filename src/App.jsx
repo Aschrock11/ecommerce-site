1c72026d7542e979f8d8cebfcc3db896dab48a27
Adam <schrock-adam@hotmail.com>
2023-03-22 23:39:07 -0400

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div>
-      {/* Header */}
+      <Header />
       {/* Categories section */}
       {/* Featured Products section */}
       {/* Shop now card (Photo and description) */}
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { ShoppingCartIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <div>
-      <p>Categories</p>
+    <div className=" sticky top-0 z-50 flex justify-between items-center">
+      <div className="  flex justify-center items-center sm:h-7 sm:w-10 lg:h-10 lg:w-13  ">
+        <img src="./images/currentFurnishLogo.png" alt="" />
+      </div>
+      <div className="flex gap-3">
+        <a href="">Home</a>
+        <a href="">Categories</a>
+        <ShoppingCartIcon className="h-6 w-6 cursor-pointer" />
+      </div>
     </div>
   );
 }
