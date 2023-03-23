61e52588847471412f01f03eddcc448111d9c4cb
Adam <schrock-adam@hotmail.com>
2023-03-22 21:41:17 -0400

    commit to header branch

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <p>Categories</p>
+    <div className=" flex justify-between items-center ">
+      <div className=" border-solid border-black  rounded-xl border-2 w-20 h-20   ">
+        LOGO gotta create it
+      </div>
+      <div>
+        <a href="">Categories</a>
+      </div>
     </div>
   );
 }
