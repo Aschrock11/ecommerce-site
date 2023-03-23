b6808925b6bd7ff1ae92d4ed64844b0f623f250c
Adam <schrock-adam@hotmail.com>
2023-03-23 14:11:42 -0400

    added simple dropdown menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import { ShoppingCartIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className=" sticky top-0 z-50 flex justify-between items-center">
-      <div className="  flex justify-center items-center sm:h-7 sm:w-10 lg:h-10 lg:w-13  ">
-        <img src="./images/currentFurnishLogo.png" alt="" />
+    <div className=" sticky top-0 z-50 flex justify-between items-center max-w-1440px h-10 w-full">
+      <div className="  flex justify-center items-center max-h-15 min-h-5 object-cover overflow-hidden  ">
+        <img
+          className=" object-contain overflow-hidden max-h-12 md:max-h-16 lg:max-h-20"
+          src="./images/currentFurnishLogo.png"
+          alt=""
+        />
       </div>
-      <div className="flex gap-3">
-        <a href="">Home</a>
-        <a href="">Categories</a>
+      <nav className="flex gap-3 lg:gap-9 md:text-xl lg:text-2xl items-center h-10">
+        <a onClick={() => setMenuOpen(!menuOpen)} href="">
+          Categories
+        </a>
+        {menuOpen && (
+          <div className=" absolute flex flex-col border-solid border-2 mt-52 w-20 md:w-[95px] lg:w-28">
+            <ul>
+              <li>Sofas</li>
+              <li>Dining</li>
+              <li>Lighting</li>
+              <li>Outdoor</li>
+              <li>Bedroom</li>
+            </ul>
+          </div>
+        )}
+
         <ShoppingCartIcon className="h-6 w-6 cursor-pointer" />
-      </div>
+      </nav>
     </div>
   );
 }
