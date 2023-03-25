89024636c37ec208f245e4685a5b02815f0df39c
Adam <schrock-adam@hotmail.com>
2023-03-24 21:25:19 -0400

    created fp component and card

---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProductCard() {
+  return (
+    <div className="border-solid border-1 border-black bg-slate-200 h-40 w-40">
+      <img src="" alt="" />
+      <p>description</p>
+      <p>price</p>
+    </div>
+  );
+}
+
+export default ProductCard;
